9eb70cd47ca619c5c5b76b4ed1c4293c338199be
ToanNguyen-exe <nguyenductoan.vtacl09@gmail.com>
2025-11-10 16:00:33 +0700

    first commit

---
+++ module 2/src/module2/LaiXuatNganHang.java
@@ -0,0 +1,26 @@
+package module2;
+
+import java.util.Scanner;
+
+public class LaiXuatNganHang {
+    public static void main(String[] args) {
+        double money = 1.00;
+        int month = 1;
+        double interestRate = 1.0;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter investment amount: ");
+        money = input.nextDouble();
+        System.out.print("Enter number of month: ");
+        month = input.nextInt();
+        System.out.println("Enter annual interest rate in percentage: ");
+        interestRate = input.nextDouble();
+        double total = 0;
+        for (int i = 0; i < month; i++) {
+            total += money * (interestRate / 100) / 12 * month;
+        }
+        System.out.println("Total interest rate: " + total);
+    }
+
+
+
+}
