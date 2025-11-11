2a42964c30ccb65c91937c68a2d45a329a10185f
ToanNguyen-exe <nguyenductoan.vtacl09@gmail.com>
2025-11-11 09:44:18 +0700

    Add files via upload

---
+++ module 2/src/module2/HienThiSoNguyenToNhoHon100.java
@@ -0,0 +1,23 @@
+package VongLapTrongJava;
+
+public class HienThiSoNguyenToNhoHon100 {
+    public static boolean isPrime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Cac so nguyen to tu 2 den 100 la:");
+
+        for (int i = 2; i <= 100; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
